26fc4e95c05828caf4c796145164310df24f3814
Andrew Tedjapratama <122066488+Fozzzzy@users.noreply.github.com>
2024-07-27 20:08:04 +0700

    fix: removed routes as it has been added in express server file

--- auth.js
+++ auth.js
@@ -11,27 +11,10 @@ function authenticateToken(req, res, next) {
     if (!token) return res.status(401).json({ message: 'Access denied' });
 
     jwt.verify(token, secret_key, (err, user) => {
-        if (err) return res.status(403);
+        if (err) return res.status(403).json({ message: 'Invalid token' });
         req.user = user;
         next();
     });
 }
 
 module.exports = { generateToken, authenticateToken };
-
-// Protected routes for each HTTP method
-router.get('/protected', authenticateToken, (req, res) => {
-    res.json({ message: 'This is a protected route', user: req.user });
-  });
-  
-  router.post('/protected', authenticateToken, (req, res) => {
-    res.json({ message: 'Protected POST route', data: req.body });
-  });
-  
-  router.put('/protected/:id', authenticateToken, (req, res) => {
-    res.json({ message: 'Protected PUT route', id: req.params.id, data: req.body });
-  });
-  
-  router.delete('/protected/:id', authenticateToken, (req, res) => {
-    res.json({ message: 'Protected DELETE route', id: req.params.id });
-  });
